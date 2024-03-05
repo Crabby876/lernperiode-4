a0bc3c9ed0c24bccea7ef9178c3629613c151d94
Doruk <142886360+Crabby876@users.noreply.github.com>
2024-03-05 11:19:21 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace API_requests_test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            using(var client = new HttpClient())
+            {
+                var endpoint = new Uri("https://catfact.ninja/fact");
+                var result = client.GetAsync(endpoint).Result.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
